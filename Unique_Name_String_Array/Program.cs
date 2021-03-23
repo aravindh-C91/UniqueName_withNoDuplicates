0e9b4fb6a07643ecd7f1442924ac3db06e12c2aa
aravindh-C91 <81221718+aravindh-C91@users.noreply.github.com>
2021-03-23 14:38:52 +0530

    Unique names with No duplicates

---
+++ Unique_Name_String_Array/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Linq;
+
+public class MergeNames
+{
+	public static string[] UniqueNames(string[] names1, string[] names2)
+	{
+		var allnames = names1.Union(names2).ToArray();
+
+		string[] distinctnames = allnames.Distinct(StringComparer.CurrentCultureIgnoreCase).ToArray();
+
+		Array.Sort(distinctnames, StringComparer.InvariantCulture);
+		
+		return distinctnames;
+
+	}
+	public static void Main(string[] args)
+	{
+		string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
+		string[] names2 = new string[] { "Olivia", "Sophia", "Emma", "ava" };
+		Console.WriteLine(string.Join(", ", UniqueNames(names1, names2))); 
+	}
+}
